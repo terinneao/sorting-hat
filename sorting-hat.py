d4d5980a1f646283414cd4f5d2d6a2daf5676c3f
terinneao <toddi1978@hotmail.com>
2022-04-27 14:21:39 +0700

    Create sorting-hat.py

---
+++ sorting-hat.py
@@ -0,0 +1,28 @@
+import random
+student_list = ["Mark", "Amber", "Todd", "Anita", "Sandy", "Mike", "John", "Mary", "Stephen", "Sam",
+                "Ann", "Lisa", "Adam", "Jerrard", "Brook", "Dan", "Don", "May", "Boy", "Nat",
+                "Art", "Tom", "Judy", "Mick", "Joy", "Brad", "David", "Alex", "Tum", "Not",
+                "Big", "Jom", "Tong", "Top", "Samuel", "Sea", "Sun", "Fluke", "Max", "Patt",
+                "Peter", "Josh", "Kathy", "Golf", "Kavin", "Roy", "Yo", "Luka", "Lucy", "Beck"            
+               ]
+
+Gryffindor = []
+Hufflepuff = []
+Ravenclaw = []
+Slytherin = []
+house_list = [Gryffindor, Hufflepuff, Ravenclaw, Slytherin]
+house_list2= [Gryffindor, Hufflepuff, Ravenclaw, Slytherin]
+house_namelist = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+        
+
+for student in student_list:
+    house = random.choice(house_list)
+    if len(house) > 12:
+        house_list.remove(house)
+        house = random.choice(house_list)
+    else:
+        pass
+    house.append(student)
+        
+for (value1, value2) in enumerate(zip(house_namelist, house_list2)): 
+    print(value2[0] +" has "+ str(len(value2[1])) +" students")
